9fc9e1df6760e79da10435d9a6d02e94a03a13cd
Alejandro Vera <alejandroverah97@gmail.com>
2020-11-30 23:33:05 -0500

    Pagina de selecion de idiomas

---
+++ idioma.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table width="25%" border="0" align="center">
+        <tr>
+            <td colspan="2" align="center"><h1>Elige idioma</h1></td>
+        </tr>
+        <tr>
+            <td align="center"><a href="creaCookie.php?idioma=es"><img src="español.png" width="90" height="60"></td></a>
+            <td align="center"><a href="creaCookie.php?idioma=in"><img src="ingles.jpg" width="90" height="60"></td></a>
+        </tr>    
+    </table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+</body>
+</html>
---
+++ ver_cookie.php
@@ -0,0 +1,13 @@
+<?php
+//ver si creamos o no la cookie
+
+if(!$_COOKIE["idioma_seleccionado"]){
+    header("Location:idioma.php");
+} 
+else if ($_COOKIE["idioma_seleccionado"]=="es") {
+    header("Location:spanish.php");
+}else if($_COOKIE["idioma_seleccionado"]=="in") {
+    header("Location: english.php");
+}
+
+?>
