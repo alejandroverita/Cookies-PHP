82d738d87779dcb53e0f978f2e1c65ed76a712c7
Alejandro Vera <alejandroverah97@gmail.com>
2020-11-30 22:45:39 -0500

    Incluyendo tiempo de vida de cookie y destruyendo

--- cookie1.php
+++ cookie1.php
@@ -1,7 +1,7 @@
 <?php
 
-//nombre de cookie  y valor 
-setcookie("prueba", "Esta es la informacion de nuestra cookie");
+//nombre de cookie  y valor. Duracion de cookie en segundos
+setcookie("prueba", "Esta es la informacion de nuestra cookie", time()+30);
 
 
 
---
+++ destroycookie.php
@@ -0,0 +1,10 @@
+<?php
+
+//con el -1 pa que borre al cerrar navegador
+setcookie("prueba", "Esta es la informacion de nuestra cookie", time()-1);
+
+
+
+
+
+?>
